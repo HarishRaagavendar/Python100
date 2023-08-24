2184638352ad2d4065679066d17a538c27f47735
HarishRaagavendar <119796099+HarishRaagavendar@users.noreply.github.com>
2023-08-24 05:10:17 +0000

    Stonepaperscissor

---
+++ rockpaperscissor.py
@@ -0,0 +1,11 @@
+#given string
+string1="Python is great"
+ 
+#printing the string
+print("Actual String: ",string1) 
+   
+#gives us the type of string1
+print("Type of string: ",type(string1))  
+ 
+print("String converted to list :",string1.split()) 
+#prints the list given by split()
